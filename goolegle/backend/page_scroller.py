0894e212b8c856cb3e43621fb467429ede0551fe
Hikari <37006149+hikarimn@users.noreply.github.com>
2020-07-28 20:21:06 -0400

    Update page_scroller.py

--- goolegle/backend/page_scroller.py
+++ goolegle/backend/page_scroller.py
@@ -1,9 +1,8 @@
-
-
 import re
 import urllib.request
 
-# from concurrent.futures import ThreadPoolExecutor
+
+from concurrent.futures import ThreadPoolExecutor
 import requests
 
 class Page_scroller:           
@@ -14,8 +13,15 @@ class Page_scroller:
         self.original_url = original_url
         self.results = {}
         self.max_threads = threads
-        self.limits = limits
+        self.limits = int(limits)
         self.url_counter = 0
+        self.regex = re.compile(
+            r'^(?:http|ftp)s?://' # http:// or https://
+            r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
+            r'localhost|' #localhost...
+            r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
+            r'(?::\d+)?' # optional port
+            r'(?:/?|[/?]\S+)$', re.IGNORECASE)
 
     # def __make_request(self, url):
     #     try:
@@ -71,8 +77,96 @@ class Page_scroller:
     #                 # i += 1
     #     return self.urls
 
+    def _wrapper(self, url):
+        self._scraper(url)
+
+    def _validate_url(self, url):
+        '''
+        check if the url submitted is valid or not
+        If it is, scraper runs
+        Otherwise, it returns an error result
+        '''
+        # print('here')
+        # print(re.match(self.regex, url) != None)
+        return re.match(self.regex, url) != None
+
+    def _scraper(self, url):
+        if len(self.urls) >= self.limits:
+            pass
+        if not self._validate_url(url):
+            pass
+        try:    
+            response = urllib.request.urlopen(url)
+            html = response.read().decode('utf-8')
+            links = set([link[0] for link in re.findall('"((http|ftp)s?://.*?)"', html)])
+            # print('-------------------------url--------------------------------')
+            # print(url)
+            # print('-----------------newly added links--------------------------')
+            # print(links)
+            # print('------------------------------------------------------------')
+            self.urls = list(set(self.urls.extend(list(links))))
+        except (UnicodeDecodeError, ValueError, Exception) as e:
+            # print(url)
+            # print(e)
+            pass
+    
+    def run_scraper(self):
+        # print('validate!')
+        # print(self._validate_url(self.original_url))
+        # print(not self._validate_url(self.original_url))
+        if not self._validate_url(self.original_url):
+            # print(self.original_url)
+            return self.urls
+        self.urls.append(self.original_url)
+
+        # print(self.limits)
+        # print(len(self.urls))
+        # print(self.urls)
+        
+        while self.limits >= len(self.urls):
+            with ThreadPoolExecutor(max_workers=self.max_threads) as executor:
+                executor.map(self._wrapper, self.urls)
+                # for url in self.urls:
+                #     print('-------------------------url--------------------------------')
+                #     print(url)
+                #     # scraped_urls = self._scraper(url)
+                #     jobs = [Executor.submit(self._wrapper, url)]
+                #     print('-------------------self.urls--------------------------------')
+                #     print(self.urls)
+                #     # print('------------------------------------------------------------')
+                #     # if scraped_urls:
+                #         # self.urls.extend(scraped_urls)
+                #     print('-------------------length--------------------------------')   
+                #     print(len(self.urls))
+                #     if len(self.urls) >= self.limits:
+                #         break
+                if len(self.urls) >= self.limits:
+                    break
+            if len(self.urls) >= self.limits:
+                    break
+        # with ThreadPoolExecutor(max_workers=self.max_threads) as Executor:
+        #     while self.limits >= len(self.urls):
+        #         for url in self.urls:
+        #             print('-------------------------url--------------------------------')
+        #             print(url)
+        #             # scraped_urls = self._scraper(url)
+        #             jobs = [Executor.submit(self._wrapper, url)]
+        #             print('-------------------self.urls--------------------------------')
+        #             print(self.urls)
+        #             # print('------------------------------------------------------------')
+        #             # if scraped_urls:
+        #                 # self.urls.extend(scraped_urls)
+        #             print('-------------------length--------------------------------')   
+        #             print(len(self.urls))
+        #             if len(self.urls) >= self.limits:
+        #                 break
+        #         if len(self.urls) >= self.limits:
+        #             break
+        return self.urls
+
+
     def run_script(self):
-        import bs4
+         
         self.urls.append(self.original_url)
 
         while self.limits >= len(self.urls):
