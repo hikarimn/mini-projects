22b94c28c1a15e120037bd3511491bf566c5ae1d
Hikari <37006149+hikarimn@users.noreply.github.com>
2020-08-01 18:39:59 -0400

    Create TimeCalculator.java

---
+++ time_adder/TimeCalculator.java
@@ -0,0 +1,10 @@
+
+ public class TimeCalculator{
+    public static void main(String[] args){
+        Time time = new Time();
+        System.out.println(time.AddMinutes("9:13 AM", 200));
+    }
+}
+
+// javac TimeCalculator.java
+// java TimeCalculator
